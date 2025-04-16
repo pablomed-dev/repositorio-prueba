029a86b8dcbf5c87c462077f7a8507ac2dba1c60
pablomed-dev <pmeldina1993@gmail.com>
2025-04-16 14:21:25 -0300

    Nuevos ejercicios en python

--- practica_1.py
+++ practica_1.py
@@ -16,15 +16,32 @@
 
 # Función que recibe una cadena de texto y devuelve un diccionario con la cantidad de veces que aparece cada letra.--------------
 
-def cantidad_letras(cadena_texto):
-    resultado = {}
-    cadena_texto_minusculas = cadena_texto.lower()
-    for letra in cadena_texto_minusculas:
-        if letra != " " and letra not in resultado:
-            cantidad_veces  = cadena_texto_minusculas.count((letra))
-            resultado[letra] = cantidad_veces
+# def cantidad_letras(cadena_texto):
+#     resultado = {}
+#     cadena_texto_minusculas = cadena_texto.lower()
+#     for letra in cadena_texto_minusculas:
+#         if letra != " " and letra not in resultado:
+#             cantidad_veces  = cadena_texto_minusculas.count((letra))
+#             resultado[letra] = cantidad_veces
 
-    return resultado
+#     return resultado
 
 
-print(f"Conteo de cantidad de letras {cantidad_letras("Hola Mundo")}")
+# print(f"Conteo de cantidad de letras {cantidad_letras("Hola Mundo")}")
+
+#Función que reciba una lista de números y devuelva una nueva lista con los números que son múltiplos de 3 y mayores que 10.-----
+
+def fn_lista_multiplo_3(lista_numero):
+    new_list = []
+    for numero in lista_numero:
+        if numero % 3 == 0 and numero > 10 :
+            new_list.append(numero)
+    return new_list
+
+def fn_lista_multiplo_3_comprimida(lista_numero):
+    return [numero for numero in lista_numero if numero % 3 == 0 and numero > 10]
+
+print(f"Números que son múltiplos de 3 y mayores que 10: {fn_lista_multiplo_3([3, 9, 12, 15, 7, 18, 21, 5, 30])}")
+print(f"Números que son múltiplos de 3 y mayores que 10: {fn_lista_multiplo_3_comprimida([3, 9, 12, 15, 7, 18, 21, 5, 30])}")
+
+
