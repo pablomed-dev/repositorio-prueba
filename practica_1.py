e8cc08c400d4b0330e5131fe1f8361abd923f432
pablomed-dev <pmeldina1993@gmail.com>
2025-04-16 12:53:10 -0300

    Ajuste para mejorar función de la práctica 1

--- practica_1.py
+++ practica_1.py
@@ -1,10 +1,15 @@
 #Función que reciba una lista de números y devuelva otra lista con los números pares multiplicados por 2.
-def lista_par_multiplicado(lista):
-    new_list= []
-    for x in lista :
-        if x % 2 == 0 :
-            new_list.append(x*2)
-    return new_list
+# def lista_par_multiplicado(lista):
+#     new_list= []
+#     for x in lista :
+#         if x % 2 == 0 :
+#             new_list.append(x*2)
+#     return new_list
     
 
-print(f"La lista es {lista_par_multiplicado([1,2,3,4,5,6])}")
+# print(f"La lista es {lista_par_multiplicado([1,2,3,4,5,6])}")
+
+def lista_par_multiplicado(lista):
+    return [x*2 for x in lista if x % 2 ==0]
+
+print(f"La nueva lista es {lista_par_multiplicado([1,2,3,4,5,6,7,8])}")
